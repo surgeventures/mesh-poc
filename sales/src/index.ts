159be11d7101d999be44f7b922b5ed9006b3cf4b
Kamil Kowalski <kamil@fresha.com>
2023-11-03 17:55:41 +0100

    Setup sales service

---
+++ sales/src/index.ts
@@ -0,0 +1,13 @@
+import { createYoga } from "graphql-yoga";
+import { schema } from "./schema";
+
+const yoga = createYoga({ schema });
+
+const server = Bun.serve({ fetch: yoga });
+
+console.info(
+  `Server is running on ${new URL(
+    yoga.graphqlEndpoint,
+    `http://${server.hostname}:${server.port}`
+  )}`
+);
