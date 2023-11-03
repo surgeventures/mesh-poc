e877e9825d1ce0eb3bb33d7b3445d5ac19818dd9
Kamil Kowalski <kamil@fresha.com>
2023-11-03 22:36:31 +0100

    Add correct API response

--- appointments/src/app.controller.ts
+++ appointments/src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
+
+class Hello {
+  @ApiProperty()
+  message: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('hello')
-  getHello(): { message: string } {
+  @ApiOkResponse({
+    status: 200,
+    description: 'Greets people',
+    type: Hello,
+  })
+  getHello() {
     return this.appService.getHello();
   }
 }
