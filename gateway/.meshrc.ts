005157d69609f3316a99f0e6d09ac8111e7dbdd4
masiulis <karolis.masiulis@ugnis.com>
2023-11-22 15:40:43 +0200

    Add type merging example

---
+++ gateway/.meshrc.ts
@@ -0,0 +1,66 @@
+import { YamlConfig } from "@graphql-mesh/types";
+
+const a: YamlConfig.Config = {
+  serve: {
+    port: 3000,
+  },
+  sources: [
+    {
+      name: "Appointments",
+      handler: {
+        openapi: {
+          endpoint: "http://localhost:3001/",
+          source: "http://localhost:3001/api-json",
+        },
+      },
+      transforms: [
+        {
+          prefix: {
+            mode: "bare",
+          },
+        },
+      ],
+    },
+    {
+      name: "Customers",
+      handler: {
+        grpc: {
+          endpoint: "localhost:3002",
+          source: "../customers/src/customers/customers.proto",
+          prefixQueryMethod: ["find"],
+        },
+      },
+    },
+    {
+      name: "Sales",
+      handler: {
+        graphql: {
+          endpoint: "http://localhost:3003/graphql",
+        },
+      },
+    },
+  ],
+  additionalTypeDefs: `
+    extend type Appointments_Appointment {
+      customer: customers__Customer @resolveTo(
+        sourceName: "Customers",                                
+        sourceTypeName: "Query",                               
+        sourceFieldName: "customers_CustomersService_FindOneByAppointmentId",
+        requiredSelectionSet: "{ id }", # This does not seem to be doing anything              
+        sourceArgs: {                                         
+          input: {
+            appointmentId: "{root.id}"
+          }
+        }
+    )
+    }
+  `,
+  documents: ["./documents/*.graphql"],
+};
+
+// const b = {
+//   sources: [Appointments],
+//   additionalTypeDefs: [generateTypeDefs(Appointments)],
+// };
+
+export default a;
