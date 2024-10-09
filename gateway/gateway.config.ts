0af8cea70ee0d64ecc18b26bdd48630fc6fc7612
Kamil Kowalski <kamil@fresha.com>
2024-10-09 15:15:19 +0200

    Add tracing to gateway

--- gateway/gateway.config.ts
+++ gateway/gateway.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from "@graphql-hive/gateway";
+import { createOtlpGrpcExporter, defineConfig } from "@graphql-hive/gateway";
 
 export const gatewayConfig = defineConfig({
   supergraph: "./supergraph.graphql",
   batching: {
     limit: 5,
+  },
+  maskedErrors: false,
+  openTelemetry: {
+    serviceName: "gateway-poc",
+    exporters: [
+      createOtlpGrpcExporter({
+        url: "http://0.0.0.0:4317",
+      }),
+    ],
   },
 });
