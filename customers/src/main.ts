c28e2de7db01c1d250fcf685621e23807df422d9
Kamil Kowalski <kamil@fresha.com>
2024-10-11 16:09:47 +0200

    Add debug logging of the request to sales and customers

--- customers/src/main.ts
+++ customers/src/main.ts
@@ -1,12 +1,13 @@
 import "dd-trace/init";
 import { createServer } from "node:http";
+import { inspect } from "node:util";
 import { createYoga, type Plugin } from "graphql-yoga";
 import { schema } from "./schema";
 
 function useDebug(): Plugin {
   return {
     onParse: ({ context }) => {
-      console.debug(context.params);
+      console.debug(inspect(context.params, false, null, true));
     },
   };
 }
